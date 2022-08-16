1c3e41a2a1cd80f4ce00b262db7352506baf89f9
Inna K <kuklina.inn@yandex.ru>
2022-08-16 17:30:47 +0300

    add README.md

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[]{3, 2, 5});
+        System.out.println(salesManager.max());
+    }
+}
