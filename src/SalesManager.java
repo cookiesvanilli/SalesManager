116ca76db25291ddf94922896f52c8b164543465
Inna K <kuklina.inn@yandex.ru>
2022-08-18 22:13:25 +0300

    add average

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,39 @@
 public class SalesManager {
-    protected long[] sales;
+    protected int[] sales;
 
-    public SalesManager(long[] sales) {
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    public long max() {
-        long max = -1;
-        for (long sale : sales) {
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
+    }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int average() {
+        int average = 0;
+        if (sales.length > 0) {
+            int sum = 0;
+            for (int sale : sales) {
+                sum += sale;
+            }
+            average = (sum - max() - min()) / sales.length;
+        }
+        return average;
     }
 }
